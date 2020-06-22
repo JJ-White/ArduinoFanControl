c1dbcc47bc7fb5ba8f25c7406e488e990ee7b4d7
Juriaan <juriaanmoonen@hotmail.com>
2020-06-22 20:50:20 +0200

    Fix NTC reading
    Most of the confusion came from a difference in voltages for the divider and ADC reference, fixed by supplying 12V to VIN instead of 5V.

--- ArduinoFanControl/ArduinoFanControl.ino
+++ ArduinoFanControl/ArduinoFanControl.ino
@@ -1,35 +1,23 @@
 
 /*
- * Info for reading NTC and converting to temperature:
- * https://www.digikey.com/en/maker/projects/how-to-measure-temperature-with-an-ntc-thermistor/4a4b326095f144029df7f2eca589ca54
- * For calculating NTC Beta from calibration readings:
- * https://www.thinksrs.com/downloads/programs/therm%20calc/ntccalibrator/ntccalculator.html
- */
+   Info for reading NTC and converting to temperature:
+   https://www.digikey.com/en/maker/projects/how-to-measure-temperature-with-an-ntc-thermistor/4a4b326095f144029df7f2eca589ca54
+   For calculating NTC Beta from calibration readings:
+   https://www.thinksrs.com/downloads/programs/therm%20calc/ntccalibrator/ntccalculator.html
+*/
 
-const int ntc_pin = A7;
+
+// NTC constants
+const int ntc_pins[] = { A0, A6, A7 };
 const int ntc_r1 = 6800;
 const float ntc_beta = 3306.17;
 const float ntc_cal_temp = 39.3 + 273.15;
 const float ntc_cal_res = 6000.0;
 
-int adc_to_temp(int adc_val){
-  Serial.print("adc: ");
-  Serial.print(adc_val);
- 
-  float volt = (float)map(adc_val, 0, 1023, 0, 5000) / 1000.0;
-  Serial.print(" volt: ");
-  Serial.print(volt);
-  
-  float r2 = ((float)volt * (float)ntc_r1) / (5.0 - (float)volt);
-  Serial.print(" r2: ");
-  Serial.print(r2);
-  
-  float temp = (ntc_beta * ntc_cal_temp) / (ntc_beta + (ntc_cal_temp * log(r2 / ntc_cal_res)));
-  temp -= 273.15; 
-  Serial.print(" temp: ");
-  Serial.print(temp);
-  Serial.println("");
-  return temp;
+float adc_to_temp(int ntc_pin) {
+  int r2 = ntc_r1 * (1023 / (1023 - (float)analogRead(ntc_pin)) - 1);
+  if ( r2 == 0 ) return 0.0;
+  else return (ntc_beta * ntc_cal_temp) / (ntc_beta + (ntc_cal_temp * log(r2 / ntc_cal_res))) - 273.15;
 }
 
 void setup() {
@@ -38,7 +26,13 @@ void setup() {
 }
 
 void loop() {
-  int raw = analogRead(ntc_pin);
-  adc_to_temp(raw);
+  Serial.print("NTC0: ");
+  Serial.print(adc_to_temp(ntc_pins[0]));
+  Serial.print(" NTC1: ");
+  Serial.print(adc_to_temp(ntc_pins[1]));
+  Serial.print(" NTC2: ");
+  Serial.print(adc_to_temp(ntc_pins[2]));
+  Serial.println("");
+
   delay(500);
 }
